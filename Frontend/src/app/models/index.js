89819aae693f21563774f38cf2875b1b62669cec
Moritz Sternemann <me@moritzsternemann.de>
2017-05-19 13:46:29 +0200

    Added model index

---
+++ Frontend/src/app/models/index.js
@@ -0,0 +1,13 @@
+import Scholar from './Scholar'
+import ScholarSocialMedia from './ScholarSocialMedia'
+import User from './User'
+import WWDCYear from './WWDCYear'
+import WWDCYearInfo from './WWDCYearInfo'
+
+export {
+  Scholar,
+  ScholarSocialMedia,
+  User,
+  WWDCYear,
+  WWDCYearInfo
+}
