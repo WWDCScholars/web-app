a27fdf7e56212afe0b654e2c4577f89d51c9ee6a
Moritz Sternemann <me@moritzsternemann.de>
2017-05-22 01:58:55 +0200

    Added color to header links

--- Frontend/src/app/App.vue
+++ Frontend/src/app/App.vue
@@ -2,10 +2,11 @@
 #app
   page-header
     .header-links(slot="right")
-      router-link(:to="{ name: 'scholars' }", :class="routeClass('scholars')"): span Scholars
-      router-link(:to="{ name: 'blog' }", :class="routeClass('blog')"): span Blog
-      router-link(:to="{ name: 'team' }", :class="routeClass('team')"): span Team
-      a(href="#"): span My Profile
+      router-link(:to="{ name: 'scholars' }", :class="routeClass('scholars')").color-purple: span Scholars
+      router-link(:to="{ name: 'blog' }", :class="routeClass('blog')").color-red: span Blog
+      router-link(:to="{ name: 'blog' }", :class="routeClass('store')").color-blue: span Store
+      router-link(:to="{ name: 'team' }", :class="routeClass('team')").color-red: span Team
+      a(href="#").color-green: span My Profile
 
   router-view
 </template>
