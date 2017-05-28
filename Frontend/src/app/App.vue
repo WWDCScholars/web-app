3d2665b409b0b29c5839c49e219ce70621cd304e
Moritz Sternemann <me@moritzsternemann.de>
2017-05-29 01:52:19 +0200

    Hide non-finished nav items

--- Frontend/src/app/App.vue
+++ Frontend/src/app/App.vue
@@ -4,10 +4,10 @@
     .header-links(slot="right")
       router-link(:to="{ name: 'scholars' }", :class="routeClass('scholars')").color-purple: span Scholars
       router-link(:to="{ name: 'activity' }", :class="routeClass('activity')").color-orange: span Activity
-      router-link(:to="{ name: 'blog' }", :class="routeClass('blog')").color-red1: span Blog
+      //- router-link(:to="{ name: 'blog' }", :class="routeClass('blog')").color-red1: span Blog
       router-link(:to="{ name: 'store' }", :class="routeClass('store')").color-blue1: span Store
-      router-link(:to="{ name: 'team' }", :class="routeClass('team')").color-red2: span Team
-      a(href="#").color-green: span My Profile
+      //- router-link(:to="{ name: 'team' }", :class="routeClass('team')").color-red2: span Team
+      //- a(href="#").color-green: span My Profile
 
   router-view
 </template>
