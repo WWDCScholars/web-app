8f9b29fa44dd64836ed5aa78efa1afd932af84f6
Moritz Sternemann <me@moritzsternemann.de>
2017-05-29 01:21:16 +0200

    Improved store text

--- Frontend/src/app/pages/Store.vue
+++ Frontend/src/app/pages/Store.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
 .page-store.section-accent-color-blue
-  tab-bar(name="store_section", :tabs="['T-Shirts']", initial="T-Shirts", @change="onTabChange")
+  tab-bar(name="store_section", :tabs="['T-shirts']", initial="T-shirts", @change="onTabChange")
 
   .container.container-outer
     .container-split-70
       h3 Want everyone to know you're a Scholarship winner?
       p.
-        Show support for the project and look good in the process with one of our 2017 t-shirts. We offer various unisex sizes in two different styles, featuring our WWDCScholars logo on front and text around back.
+        Show support for the project and look good in the process with one of our 2017 T-shirts. We offer various unisex sizes in two different styles, featuring our WWDCScholars logo on front and text around back.
 
       .image-slider
         swiper(:options="swiperOptions")
@@ -17,13 +17,13 @@
           .swiper-button-prev.swiper-button(slot="button-prev")
           .swiper-button-next.swiper-button(slot="button-next")
 
-      h3 How do I order and recieve my t-shirt?
+      h3 How do I order and recieve my T-shirt?
       p.
-        Simply complete the DonorBox form on the right of this page and we will get your t-shirt prepared.
+        Simply complete the DonorBox form on the right of this page and we will get your T-shirt prepared.
       p.
-        If you are ordering in advance of WWDC, we will contact you at the start of the week with details of where and we will be handling out orders.
+        If you are ordering in advance of WWDC, we will contact you at the start of the event with details of where and we will be handling out orders.
       p.
-        A similar process applies for ordering during WWDC itself. We will be in contact shortly after you complete your order and you can collect your t-shirt from us at an arranged time and location.
+        A similar process applies for ordering during WWDC itself. We will be in contact shortly after you complete your order and you can collect your T-shirt from us at an arranged time and location.
       p.
         #[b Please note:] T-shirts are currently available for collection at WWDC only.
 
