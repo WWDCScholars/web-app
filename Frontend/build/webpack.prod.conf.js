b997b3b599549be0bdce5925259f177f43c379fd
Moritz Sternemann <me@moritzsternemann.de>
2017-05-30 17:40:22 +0200

    Fixed production config

--- Frontend/build/webpack.prod.conf.js
+++ Frontend/build/webpack.prod.conf.js
@@ -119,7 +119,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // copy custom static assets
     new CopyWebpackPlugin([
       {
-        from: path.resolve(__dirname, '../src/assets/static'),
+        from: path.resolve(__dirname, '../src/static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
