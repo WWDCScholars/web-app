e137c6c08956558df95b69cfda92ed4a826c6bf7
michie <veryhappymichie@gmail.com>
2018-05-16 21:53:17 +0800

    Update shirt details for WWDC 2018! It's ready for sale! :)

--- Frontend/src/app/pages/Store.vue
+++ Frontend/src/app/pages/Store.vue
@@ -3,46 +3,49 @@
   tab-bar(name="store_section", :tabs="['T-shirts']", initial="T-shirts", @change="onTabChange")
 
   .container.container-outer
-    //- .container-split-70
-    h3 Want everyone to know you're a Scholarship winner?
-    h4.color-red1 T-Shirts will be available again shortly before WWDC 2018
-    p.
-      Show support for the project and look good in the process with one of our 2018 T-shirts. We offer various unisex sizes in two different styles, featuring our WWDCScholars logo on front and text around back.
+    .container-split-70
+      h3 Want everyone to know you're a Scholarship winner?
+      h4.color-purple T-Shirts are now available until May 25, 2018!
+      p.
+        Show support for the project and look good in the process with our  #WWDCScholars T-shirts. We offer various sizes for both Unisex and Ladies style featuring our logo on front and text around back.
 
-    .image-slider
-      swiper(:options="swiperOptions")
-        swiper-slide: img(src="~images/tshirt-white-front.jpg")
-        swiper-slide: img(src="~images/tshirt-white-back.jpg")
-        swiper-slide: img(src="~images/tshirt-grey-front.jpg")
-        swiper-slide: img(src="~images/tshirt-grey-back.jpg")
-        .swiper-button-prev.swiper-button(slot="button-prev")
-        .swiper-button-next.swiper-button(slot="button-next")
+      .image-slider
+        swiper(:options="swiperOptions")
+          swiper-slide: img(src="~images/tshirt-white-front.jpg")
+          swiper-slide: img(src="~images/tshirt-white-back.jpg")
+          //- swiper-slide: img(src="~images/tshirt-grey-front.jpg")
+          //- swiper-slide: img(src="~images/tshirt-grey-back.jpg")
+          .swiper-button-prev.swiper-button(slot="button-prev")
+          .swiper-button-next.swiper-button(slot="button-next")
 
-    h3 How do I order and receive my T-shirt?
-    p.
-      Simply complete the DonorBox form on the right of this page and we will get your T-shirt prepared.
-    p.
-      If you are ordering in advance of WWDC, we will contact you at the start of the event with details of where and we will be handling out orders.
-    p.
-      A similar process applies for ordering during WWDC itself. We will be in contact shortly after you complete your order and you can collect your T-shirt from us at an arranged time and location.
-    p.
-      #[b Please note:] T-shirts are currently available for collection at WWDC only.
+      h3 How do I order and receive my T-shirt?
+      p.
+        Simply complete the form and we will get your shirt prepared.
+      p.
+        We will only accept orders until May 25, 2018 and you'll receive your shirt during WWDC. We will reach out to you via email or iMessage in order for you to get your shirt.
+      p.
+        #[b Please note:] T-shirts are currently available for collection at WWDC only.
 
-    h3 Product Information
-    table
-      tr
-        td: b Sizes
-        td S, M, L, XL
-      tr
-        td(rowspan="2"): b Style
-        td White (coloured logo, purple text)
-      tr
-        td Dark Grey (white logo, white text)
-      tr
-        td: b Gender
-        td Unisex
+      h3 Product Information
+      table
+        tr
+          td(rowspan="2"): b Sizes
+          td Unisex-Not Fitted S, M, L, XL 2XL, 3XL
+        tr
+          td Ladies-Fitted S, M, L, XL, 2XL
+        tr
+          td(rowspan="2"): b Style
+          td White (coloured logo, purple text)
 
-    //- .container-split-30
+      .image-slider
+        swiper(:options="swiperOptions")
+          swiper-slide: img(src="~images/tshirt-unisex-not-fitted-size-chart.png")
+          swiper-slide: img(src="~images/tshirt-ladies-fitted-size-chart.png")
+          swiper-slide: img(src="~images/tshirt.png")
+          .swiper-button-prev.swiper-button(slot="button-prev")
+          .swiper-button-next.swiper-button(slot="button-next")
+
+    .container-split-30
       donor-box(name="wwdcscholars-shirts")
 </template>
 
