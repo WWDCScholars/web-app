3137d7a2e69140c96468c781ac61b5b2abc403b9
Moritz Sternemann <moritz@strnmn.me>
2019-04-07 18:59:53 +0200

    Added slotting for button

--- components/BaseButton.vue
+++ components/BaseButton.vue
@@ -1,42 +1,68 @@
 <template lang="pug">
-button.btn
-  slot
+.btn
+  slot(v-if="hasNobtnSlot", name="nobtn")
+  button(v-else)
+    slot
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component
-export default class BaseButton extends Vue {}
+export default class BaseButton extends Vue {
+  get hasNobtnSlot(): boolean {
+    return !!this.$slots.nobtn
+  }
+}
 </script>
 
 <style lang="sass" scoped>
 .btn
-  display: block
-  padding: 12px 15px
-  min-width: 160px
-  border: 1px solid $form-border-color
-  border-radius: $border-radius
-  font-size: 1em
-  color: $sch-gray
-  background-color: $white
-  text-align: center
-  text-transform: none
-  transition: background-color 100ms linear, border-color 100ms linear, color 100ms linear
+  > button, a
+    display: block
+    padding: 12px 15px
+    min-width: 160px
+    border: 1px solid $form-border-color
+    border-radius: $border-radius
+    font-size: 1em
+    color: $sch-gray
+    background-color: $white
+    text-align: center
+    text-transform: none
+    text-decoration: none
+    transition: background-color 100ms linear, border-color 100ms linear, color 100ms linear
 
-  &:hover
-    background-color: darken($background-gray, 6%)
+    &:hover
+      background-color: darken($background-gray, 6%)
+
+  &.btn-round > .button, a
+    padding: 10px 15px
+    border: 2px solid $form-border-color
+    border-radius: 32px
+
+    &:hover
+      background-color: $background-gray
+      color: $white
 
 +form-colors
   $bg: dyn-temp('bg')
   $fg: dyn-temp('fg')
 
   .btn
-    background-color: $bg
-    border-color: $bg
-    color: $fg
+    > button, a
+      background-color: $bg
+      border-color: $bg
+      color: $fg
 
-    &:hover
-      background-color: darken($bg, 10%)
-      border-color: darken($bg, 10%)
+      &:hover
+        background-color: darken($bg, 10%)
+        border-color: darken($bg, 10%)
+
+    &.btn-round > button, a
+      background-color: $fg
+      color: $bg
+
+      &:hover
+        background-color: $bg
+        color: $fg
 </style>
