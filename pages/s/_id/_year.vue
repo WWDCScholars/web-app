272c742c40bd5393fd0a4ccd926c752fc6ff7531
Moritz Sternemann <moritz@strnmn.me>
2019-05-15 15:37:00 +0200

    feat: Made swiper work with different image sizes

--- pages/s/_id/_year.vue
+++ pages/s/_id/_year.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.submission(v-if="wwdcYear")
+.submission(v-if="wwdcYear && yearInfo")
   p.challenge-description
     | {{ challengeDescription }}
     span(v-if="yearInfo.description").
@@ -8,15 +8,15 @@
 
   p.description {{ yearInfo.description }}
 
-  swiper(v-if="true", :options.once="swiperOptions").screenshots
+  swiper(v-if="yearInfo.screenshots", :options="swiperOptions").screenshots
       swiper-slide(
         v-for="screenshot in yearInfo.screenshots",
         :key="screenshot.fileChecksum"
       ).screenshot
         img(:src="screenshot.downloadURL")
       .swiper-pagination(slot="pagination")
-      .swiper-buttton-prev.swiper-button(slot="button-prev")
-      .swiper-buttton-next.swiper-button(slot="button-next")
+      .swiper-button-prev.swiper-button(slot="button-prev")
+      .swiper-button-next.swiper-button(slot="button-next")
 </template>
 
 <script lang="ts">
@@ -25,6 +25,8 @@ import { namespace } from 'vuex-class'
 import { swiper, swiperSlide } from 'vue-awesome-swiper'
 import { Scholar, WWDCYear, WWDCYearInfo } from '@wwdcscholars/cloudkit'
 import yearToFetch from '~/util/wwdcYear-index'
+
+import 'swiper/dist/css/swiper.css'
 
 import * as years from '~/store/years'
 const Years = namespace(years.name)
@@ -42,23 +44,33 @@ export default class ScholarProfileSubmission extends Vue {
   yearInfo!: WWDCYearInfo
 
   swiperOptions = {
-    autoplay: 8000,
+    autoplay: {
+      delay: 8000,
+      disableOnInteraction: true
+    },
     loop: false,
     grabCursor: false,
     setWrapperSize: false,
     spaceBetween: 15,
     slidesPerView: 'auto',
-    prevButton: '.swiper-button-prev',
-    nextButton: '.swiper-button-next'
+    navigation: {
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next'
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    }
   }
 
   @Years.Getter('byRecordName') yearByRecordName
 
-  get wwdcYear(): WWDCYear {
+  get wwdcYear(): WWDCYear | undefined {
+    if (!this.yearInfo) return undefined
     return this.yearByRecordName(this.yearInfo.year.recordName)
   }
 
   get challengeDescription(): string {
+    if (!this.wwdcYear) return ''
     return this.wwdcYear.challengeDescription || ''
   }
 
@@ -107,9 +119,63 @@ export default class ScholarProfileSubmission extends Vue {
   font-size: 0.9em
 
 .screenshots
+  position: relative
+  left: -60px
+  right: -60px
+  width: calc(100% + 120px)
+  height: 300px
   margin-top: 40px
 
   .screenshot
+    width: auto
+
     img
-      width: 100%
+      height: 100%
+
+$swiper-arrow-length: 20px
+$swiper-arrow-width: 2px
+.swiper-container
+  .swiper-button
+    background: 0
+    width: $swiper-arrow-length
+    height: $swiper-arrow-length
+    margin-top: -$swiper-arrow-length / 2
+    opacity: 0
+    transition: opacity 100ms linear
+
+    &:before, &:after
+      content: ''
+      display: block
+      position: absolute
+      background-color: lighten($sch-purple, 30%)
+
+    &:before
+      width: $swiper-arrow-length
+      height: $swiper-arrow-width
+
+    &:after
+      width: $swiper-arrow-width
+      height: $swiper-arrow-length
+
+    &.swiper-button-prev
+      transform: rotate(-45deg)
+
+    &.swiper-button-next
+      transform: rotate(135deg)
+
+  /deep/.swiper-pagination-bullets
+    opacity: 0
+    transition: opacity 100ms linear
+
+    .swiper-pagination-bullet
+      background: $sch-gray3
+
+    .swiper-pagination-bullet-active
+      background: lighten($sch-purple, 30%)
+
+  &:hover .swiper-button, &:hover .swiper-pagination-bullets
+    opacity: 1
+
+    &.swiper-button-disabled
+      opacity: 0.35
 </style>
