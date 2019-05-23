619531d1399cbd991be65a68e366845709401270
Moritz Sternemann <moritz@strnmn.me>
2019-05-23 21:06:19 +0200

    chore: Add sentry release script

---
+++ .github/release_sentry.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Add node_modules/.bin to PATH
+PATH=$PATH:node_modules/.bin
+
+# Sentry organization & project
+ORG=wwdcscholars
+PRJ=app
+
+# Base URL
+URL=https://www.wwdcscholars.com
+
+# Get version (current tag)
+VERSION=`git describe --tags`
+
+# Create new version in sentry
+sentry-cli releases -o $ORG -p $PRJ new --finalize $VERSION
+sentry-cli releases -o $ORG -p $PRJ set-commits --auto $VERSION
+sentry-cli releases -o $ORG -p $PRJ files $VERSION upload-sourcemaps --no-rewrite --url-prefix $URL ./dist
