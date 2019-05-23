d48fdae4bccfe49ae2ef2ddc8238e0a57d5f617d
Moritz Sternemann <moritz@wwdcscholars.com>
2019-05-23 23:30:28 +0200

    feat: Scroll NavigationTabBar to right on mounted

--- components/NavigationTabBar.vue
+++ components/NavigationTabBar.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.tab-bar-container
+.tab-bar-container(ref="tabBar")
   tab-bar
     slot
 </template>
@@ -11,7 +11,12 @@ import TabBar from './TabBar.vue'
 @Component({
   components: { TabBar }
 })
-export default class NavigationTabBar extends Vue {}
+export default class NavigationTabBar extends Vue {
+  mounted() {
+    const tabBar = this.$refs.tabBar as Element
+    tabBar.scrollLeft = tabBar.scrollWidth
+  }
+}
 </script>
 
 <style lang="sass" scoped>
@@ -20,4 +25,10 @@ export default class NavigationTabBar extends Vue {}
   text-align: center
   background-color: $white
   box-shadow: 0 2px 4px 0 transparentize($sch-gray, 0.6)
+  overflow-x: scroll
+  position: relative
+
+  #wat
+    position: absolute
+    right: 0
 </style>
