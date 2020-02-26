ab7d20bfc93b2ce60950eb24ac4e7bbe22d2dc22
Moritz Sternemann <moritz@wwdcscholars.com>
2020-02-26 11:47:28 +0100

    feat: Wrap form fields in ValidationProvider

--- components/FormField.vue
+++ components/FormField.vue
@@ -1,27 +1,40 @@
 <template lang="pug">
-.field
-  slot
-  .input-error(v-if="error") {{ error }}
+ValidationProvider(
+  tag="div",
+  :rules="rules",
+  :name="name",
+  :vid="vid",
+  v-slot="validation"
+).field
+  slot(v-bind="validation")
+  .input-error(v-if="validation.errors[0]") {{ validation.errors[0] }}
   .comment(v-if="comment") {{ comment }}
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import { ValidationProvider } from 'vee-validate'
 
-@Component
+@Component({
+  components: { ValidationProvider }
+})
 export default class FormField extends Vue {
   @Prop({ default: '' })
-  error!: string
-  @Prop({ default: '' })
   comment!: string
+  @Prop({ default: '' })
+  rules!: string | object
+  @Prop({ required: true })
+  name!: string
+  @Prop({ default: undefined })
+  vid?: string
 }
 </script>
 
 <style lang="sass" scoped>
 .input-error
-  font-size: 0.8em
+  font-size: 0.7em
   margin-left: 2px
-  margin-top: 15px
+  margin-top: 3px
   color: $sch-red1
 
 /deep/ .comment
