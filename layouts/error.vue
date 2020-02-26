764e8c049c0950c0ac2ac9f5039a284ddd7437b4
Moritz Sternemann <moritz@wwdcscholars.com>
2020-02-26 11:42:43 +0100

    feat: Display error message

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,7 +2,9 @@
 .container-fluid
   .section
     h2(v-if="error.statusCode === 404").color-blue1 Page not found
-    h2(v-else) Unfortunately there was a problem...
+    template(v-else)
+      h2 Unfortunately there was a problem...
+      p {{ error.message }}
 
     .error-404(v-if="error.statusCode === 404")
       p(v-if="!error.message") The link you clicked may be broken or no longer exists.
