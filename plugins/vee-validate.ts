037ea10a4c345c8e6e9c142e181c85a66c5c7d2d
Moritz Sternemann <moritz@wwdcscholars.com>
2020-02-26 11:48:00 +0100

    feat: Add some custom input validation rules

--- plugins/vee-validate.ts
+++ plugins/vee-validate.ts
@@ -1,5 +1,6 @@
 import { extend, localize } from 'vee-validate'
 import { email, image, max, min, required } from 'vee-validate/dist/rules'
+import { dimensions, phoneOrEmail, required_image, url } from '~/util/validation'
 import en from 'vee-validate/dist/locale/en.json'
 
 extend('email', email)
@@ -7,6 +8,9 @@ extend('image', image)
 extend('max', max)
 extend('min', min)
 extend('required', required)
+extend('dimensions', dimensions)
+extend('phoneOrEmail', phoneOrEmail)
+extend('required_image', required_image)
 extend('url', url)
 
 localize({ en })
---
+++ util/validation/index.ts
@@ -0,0 +1,11 @@
+import dimensions from './dimensions'
+import phoneOrEmail from './phoneOrEmail'
+import required_image from './required_image'
+import url from './url'
+
+export {
+  dimensions,
+  phoneOrEmail,
+  required_image,
+  url
+}
