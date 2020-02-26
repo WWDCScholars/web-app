bb34feecf537b10e5695981dfd63cc0cdd0e4227
Moritz Sternemann <moritz@wwdcscholars.com>
2020-02-26 11:41:23 +0100

    feat: Split InputImage to InputImage and InputImageMultiple

--- components/inputs/InputImage.vue
+++ components/inputs/InputImage.vue
@@ -1,35 +1,33 @@
 <template lang="pug">
 .input-image
-  .image(v-for="i in imageCount")
+  .image
     input(
       type="file",
       :name="name",
       :accept="accept",
-      @change="onFileInputChanged(i - 1, $event)"
+      :required="required",
+      @change="onChange"
     )
-    img(src="~/assets/images/upload-icon.png").upload-icon
-    img(
-      v-if="previews[i - 1]"
-      :src="previews[i - 1]"
-    ).preview
+    slot(name="icon")
+      img(src="~assets/images/upload-icon.png").upload-icon
+    .preview-wrapper
+      img(
+        v-if="preview",
+        :src="preview"
+      ).preview
     button(
-      v-if="previews[i - 1]",
-      @click="clearField(i - 1)"
-    ).cta-remove
-
-  button(
-    v-if="multiple && imageCount < maxCount"
-    @click="addField"
-  ).cta-add
+      v-if="value",
+      @click="clear"
+    ).button-remove
 </template>
 
 <script lang="ts">
-import { Component, Model, Prop, Vue } from 'nuxt-property-decorator'
+import { Component, Model, Prop, Watch, Vue } from 'nuxt-property-decorator'
 
 @Component
 export default class InputImage extends Vue {
-  @Model('change')
-  value!: { [i: number]: File | string }
+  @Model('input')
+  value!: File | string | null
 
   @Prop({ required: true })
   name!: string
@@ -37,156 +35,119 @@ export default class InputImage extends Vue {
   accept!: string
   @Prop({ default: false })
   required!: boolean
-  @Prop({ default: false })
-  multiple!: boolean
-  @Prop({ default: 1 })
-  maxCount!: number
 
-  imageCount: number = 1
-  previews: string[] = []
-
-  // value_validate: (File | undefined)[] = this.value || [undefined]
+  preview: string | null = null
 
   created() {
-    Object.values(this.value).forEach((fileOrUrl, index) => {
-      if (typeof fileOrUrl === 'string') {
-        this.previews[index] = fileOrUrl
-      } else {
-        this.updatePreview(index, fileOrUrl)
-      }
-    })
+    this.updatePreview()
   }
 
-  addField() {
-    if (this.imageCount >= (this.maxCount || 0)) {
-      return
-    }
-
-    this.imageCount++
-  }
-
-  clearField(index) {
-    this.$set(this.previews, index, undefined)
-    delete this.value[index]
-    this.$emit('change', this.value)
-  }
-
-  onFileInputChanged(index, event) {
+  onChange(event) {
     const input = event.srcElement || event.target
-    if (!input.files || !input.files[0]) {
-      return
-    }
+    if (!input.files || !input.files[0]) return
 
     const file = input.files[0]
-
-    // update preview
-    this.updatePreview(index, file)
-
-    // add to model
-    this.value[index] = file
-
-    // emit changed model
-    this.$emit('change', this.value)
+    this.$emit('input', file)
   }
 
-  updatePreview(index, file) {
-    const reader = new FileReader()
-    reader.onload = (e) => {
-      if (!e.target) return
-      this.$set(this.previews, index, e.target['result'])
+  @Watch('value')
+  updatePreview() {
+    if (typeof this.value === 'string') {
+      this.preview = this.value
+    } else if (this.value instanceof File) {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        if (!e.target) {
+          this.preview = ''
+          return
+        }
+        this.preview = e.target['result'] as string
+      }
+      reader.readAsDataURL(this.value as File)
+    } else {
+      this.preview = null
     }
-    reader.readAsDataURL(file)
+  }
+
+  clear() {
+    this.$emit('input', null)
   }
 }
 </script>
 
 <style lang="sass" scoped>
-.input-image
-  display: block
+.image
+  position: relative
+  width: 100
+  height: 100%
+  background-color: $white
+  border: 1px solid $form-border-color
+  border-radius: $border-radius
+  cursor: pointer
+  transition: border-color 100ms linear, box-shadow 100ms linear
 
-  &.image-height-fixed
-    display: grid
-    grid-template-columns: repeat(auto-fill, 220px)
-    grid-auto-rows: 165px
-    grid-gap: 15px
-    justify-content: flex-start
-
-  .image
-    position: relative
+  input
     width: 100%
     height: 100%
-    background-color: $white
-    border: 1px solid $form-border-color
-    border-radius: $border-radius
+    opacity: 0
     cursor: pointer
-    transition: border-color 100ms linear, box-shadow 100ms linear
 
-    input
-      width: 100%
-      height: 100%
-      opacity: 0
-      cursor: pointer
+  .upload-icon
+    display: block
+    position: absolute
+    top: 50%
+    left: 50%
+    transform: translateX(-50%) translateY(-50%)
+    width: 40px
+    height: 40px
+    pointer-events: none
 
-    .upload-icon
-      display: block
-      position: absolute
-      top: 50%
-      left: 50%
-      transform: translateX(-50%) translateY(-50%)
-      width: 40px
-      height: 40px
-      pointer-events: none
+  .preview-wrapper
+    position: absolute
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+    padding: 15px
+    pointer-events: none
 
     .preview
-      position: absolute
-      top: 50%
-      left: 50%
-      width: 95px
-      height: 95px
-      transform: translateX(-50%) translateY(-50%)
-      overflow: hidden
-      pointer-events: none
+      width: 100%
+      height: 100%
       border: 0
       object-fit: cover
 
-    .cta-remove
+  .button-remove
+    position: absolute
+    top: 5px
+    right: 5px
+    width: 20px
+    height: 20px
+    border-radius: 10px
+    border: 0
+    background-color: $sch-red1
+    padding: 0
+
+    &:before, &:after
+      content: ''
       position: absolute
-      top: 5px
-      right: 5px
-      width: 20px
-      height: 20px
-      border-radius: 10px
-      border: 0
-      background-color: $sch-red1
-      padding: 0
+      top: 9px
+      left: 4px
+      width: 12px
+      height: 2px
+      border-radius: 1.5px
+      background-color: $white
 
-      &:after
-        content: ' '
-        position: absolute
-        top: 8px
-        left: 4px
-        width: 12px
-        height: 3px
-        border-radius: 1.5px
-        background-color: $white
+    &:before
+      transform: rotate(-45deg)
 
-  .cta-add
-    width: 100%
-    height: 100%
-    background-color: $white
-    border: 1px solid $form-border-color
-    border-radius: $border-radius
-    background-image: url("~assets/images/add-icon.png")
-    background-repeat: no-repeat
-    background-size: 40px 40px
-    background-position: center
-    transition: border-color 100ms linear, box-shadow 100ms linear
+    &:after
+      transform: rotate(45deg)
 
 +form-colors
   $bg: dyn-temp('bg')
   .input-image
-    .image, .cta-add
-      &:hover
-        border-color: $bg
-        box-shadow: 0 0 4px transparentize($bg, 0.6)
+    .image:hover
+      border-color: $bg
+      box-shadow: 0 0 4px transparentize($bg, 0.6)
 </style>
