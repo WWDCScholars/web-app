10550474a5051b897c50b2657281ff28931873e2
Moritz Sternemann <moritz@wwdcscholars.com>
2020-09-28 00:29:41 +0200

    fix: Scroll tab bar into view whenever it changes

--- components/NavigationTabBar.vue
+++ components/NavigationTabBar.vue
@@ -5,14 +5,31 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Watch, Vue } from 'nuxt-property-decorator'
 import TabBar from './TabBar.vue'
 
 @Component({
   components: { TabBar }
 })
 export default class NavigationTabBar extends Vue {
+  observer?: MutationObserver
+
   mounted() {
+    this.observer = new MutationObserver(() => {
+      this.scrollSelectedIntoView()
+    })
+
+    const foo = (this.$refs.tabBar as Element).children[0]
+    this.observer.observe(foo, { childList: true })
+
+    this.scrollSelectedIntoView()
+  }
+
+  beforeDestroy() {
+    this.observer?.disconnect()
+  }
+
+  private scrollSelectedIntoView() {
     const tabBar = this.$refs.tabBar as Element
     const activeLink = tabBar.getElementsByClassName('nuxt-link-exact-active')
     if (activeLink[0]) {
@@ -34,8 +51,4 @@ export default class NavigationTabBar extends Vue {
   box-shadow: 0 2px 4px 0 transparentize($sch-gray, 0.6)
   overflow-x: scroll
   position: relative
-
-  #wat
-    position: absolute
-    right: 0
 </style>
