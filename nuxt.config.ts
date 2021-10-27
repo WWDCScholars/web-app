30f0b04f7cb71a55c57e72f61996136609094978
Moritz Sternemann <moritz@wwdcscholars.com>
2021-10-27 13:45:12 +0200

    Move color-mode to runtime dependencies
    Remove unused configuration, using default options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -251,21 +251,6 @@ const config: NuxtConfig = {
         }
       }
     }
-  },
-
-
-  /*
-   ** Setup color-scheme
-  */
-  colorMode: {
-    preference: 'system', // default value of $colorMode.preference
-    fallback: 'light', // fallback value if not system preference found
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '-mode',
-    storageKey: 'nuxt-color-mode'
   }
 }
 
