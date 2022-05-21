b6a11710e07013733fc40be76f3bc9193faa7e5a
Moritz Sternemann <moritz@wwdcscholars.com>
2022-05-21 19:41:08 +0200

    feat: Add axios for link api

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,8 +1,33 @@
 import { Plugin } from '@nuxt/types'
+import { NuxtAxiosInstance } from '@nuxtjs/axios'
 
 const axiosPlugin: Plugin = ({ $config, $axios }, inject) => {
+  // second axios instance for link api
+  const linkAPI = $axios.create()
+  linkAPI.setBaseURL($config.linkApiBaseURL)
+  linkAPI.setToken($config.linkApiToken, 'Bearer')
+  inject('linkAPI', linkAPI)
+
   // default axios instance for netlify forms
   $axios.setHeader('content-type', 'application/x-www-form-urlencoded')
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $linkAPI: NuxtAxiosInstance
+  }
+}
+
+declare module '@nuxt/types' {
+  interface NuxtAppOptions {
+    $linkAPI: NuxtAxiosInstance
+  }
+}
+
+declare module 'vuex/types/index' {
+  interface Store<S> {
+    $linkAPI: NuxtAxiosInstance
+  }
 }
 
 export default axiosPlugin
