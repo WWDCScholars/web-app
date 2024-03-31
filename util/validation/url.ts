0fc8e756ae0c40eecd2c8813e3f2115616d98b3e
Moritz Sternemann <moritz@wwdcscholars.com>
2024-03-31 17:05:53 +0200

    chore: Use JS built-in URL parsing to validate URLs

--- util/validation/url.ts
+++ util/validation/url.ts
@@ -1,13 +1,13 @@
 import { RuleParamSchema } from 'vee-validate/dist/types/types'
 
 const validate = (subject: string) => {
-  const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-  return !!pattern.test(subject)
+  try {
+    new URL(subject)
+  } catch {
+    return false
+  }
+
+  return true
 }
 
 const params: RuleParamSchema[] = []
